5b41a497558672773acc6521cd391ff7da8ce1ab
Arnaud Barisain-Monrose <abarisain@gmail.com>
2016-08-22 22:50:22 +0200

    Fork old godef/godoc into another file

---
+++ src/goDeclarationCompat.ts
@@ -0,0 +1,94 @@
+/*---------------------------------------------------------
+ * Copyright (C) Microsoft Corporation. All rights reserved.
+ * Licensed under the MIT License. See License.txt in the project root for license information.
+ *--------------------------------------------------------*/
+
+'use strict';
+
+import vscode = require('vscode');
+import cp = require('child_process');
+import path = require('path');
+import { getBinPath } from './goPath';
+import { byteOffsetAt } from './util';
+import { promptForMissingTool } from './goInstallTools';
+import { GoDefinitionInformtation } from './goDeclaration';
+
+export function definitionLocation(document: vscode.TextDocument, position: vscode.Position, includeDocs = true): Promise<GoDefinitionInformtation> {
+	return new Promise<GoDefinitionInformtation>((resolve, reject) => {
+
+		let wordAtPosition = document.getWordRangeAtPosition(position);
+		let offset = byteOffsetAt(document, position);
+
+		let godef = getBinPath('godef');
+
+		// Spawn `godef` process
+		let p = cp.execFile(godef, ['-t', '-i', '-f', document.fileName, '-o', offset.toString()], {}, (err, stdout, stderr) => {
+			try {
+				if (err && (<any>err).code === 'ENOENT') {
+					promptForMissingTool('godef');
+				}
+				if (err) return resolve(null);
+				let result = stdout.toString();
+				let lines = result.split('\n');
+				let match = /(.*):(\d+):(\d+)/.exec(lines[0]);
+				if (!match) {
+					// TODO: Gotodef on pkg name:
+					// /usr/local/go/src/html/template\n
+					return resolve(null);
+				}
+				let [_, file, line, col] = match;
+				let signature = lines[1];
+				let godoc = getBinPath('godoc');
+				let pkgPath = path.dirname(file);
+				let definitionInformation: GoDefinitionInformtation = {
+					file: file,
+					line: +line - 1,
+					col: + col - 1,
+					lines,
+					doc: undefined
+				};
+				if (!includeDocs) {
+					return resolve(definitionInformation);
+				}
+				cp.execFile(godoc, [pkgPath], {}, (err, stdout, stderr) => {
+					if (err && (<any>err).code === 'ENOENT') {
+						vscode.window.showInformationMessage('The "godoc" command is not available.');
+					}
+					let godocLines = stdout.toString().split('\n');
+					let doc = '';
+					let sigName = signature.substring(0, signature.indexOf(' '));
+					let sigParams = signature.substring(signature.indexOf(' func') + 5);
+					let searchSignature = 'func ' + sigName + sigParams;
+					for (let i = 0; i < godocLines.length; i++) {
+						if (godocLines[i] === searchSignature) {
+							while (godocLines[++i].startsWith('    ')) {
+								doc += godocLines[i].substring(4) + '\n';
+							}
+							break;
+						}
+					}
+					if (doc !== '') {
+						definitionInformation.doc = doc;
+					}
+					return resolve(definitionInformation);
+				});
+			} catch (e) {
+				reject(e);
+			}
+		});
+		p.stdin.end(document.getText());
+	});
+}
+
+export class GoDefinitionProviderCompat implements vscode.DefinitionProvider {
+
+	public provideDefinition(document: vscode.TextDocument, position: vscode.Position, token: vscode.CancellationToken): Thenable<vscode.Location> {
+		return definitionLocation(document, position, false).then(definitionInfo => {
+			if (definitionInfo == null) return null;
+			let definitionResource = vscode.Uri.file(definitionInfo.file);
+			let pos = new vscode.Position(definitionInfo.line, definitionInfo.col);
+			return new vscode.Location(definitionResource, pos);
+		});
+	}
+
+}
